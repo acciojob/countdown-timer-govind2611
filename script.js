b0257d49dc0eca7e9e303ad2ab9eadf7e89eb941
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-20 08:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Your script here.
 // Get DOM elements
 const userInput = document.getElementById('userInput');
 const startButton = document.querySelector('.timer button');
@@ -13,38 +12,38 @@ function startCountdown() {
   // Disable user input and start button during the countdown
   userInput.disabled = true;
   startButton.disabled = true;
-  
+
   // Get the duration from the user input
   const duration = userInput.value * 60; // Convert minutes to seconds
-  
+
   // Calculate the end time
   const endTime = new Date(Date.now() + duration * 1000);
-  
+
   // Update the end time text
-  endTimeText.textContent = `End Time: ${formatTime(endTime)}`;
-  
+  endTimeText.textContent = `End Time: ${formatClockTime(endTime)}`;
+
   // Update the countdown every second
   const countdownInterval = setInterval(updateCountdown, 1000);
-  
+
   // Function to update the countdown
   function updateCountdown() {
     // Calculate the remaining time
     const currentTime = Date.now();
     const remainingTime = Math.round((endTime - currentTime) / 1000);
-    
+
     // Check if the countdown has ended
     if (remainingTime < 0) {
       // Clear the countdown interval
       clearInterval(countdownInterval);
-      
+
       // Update the countdown text and enable user input and start button
       countDownText.textContent = 'Countdown Ended';
       userInput.disabled = false;
       startButton.disabled = false;
-      
+
       return;
     }
-    
+
     // Update the countdown text
     countDownText.textContent = `Remaining Time: ${formatTime(remainingTime)}`;
   }
@@ -55,8 +54,18 @@ function formatTime(time) {
   const hours = Math.floor(time / 3600);
   const minutes = Math.floor((time % 3600) / 60);
   const seconds = time % 60;
-  
+
   return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+}
+
+// Function to format clock time (hh:mm AM/PM)
+function formatClockTime(time) {
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const amPm = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = hours % 12 || 12;
+
+  return `${formattedHours}:${padZero(minutes)} ${amPm}`;
 }
 
 // Function to pad zeros to single-digit numbers
